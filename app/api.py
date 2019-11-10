503bb8b1698a79df5ec0c7e8a2991b0929aebb10
Brynn <bdunbeck@gmail.com>
2019-11-10 13:36:50 -0800

    api working

--- app/api.py
+++ app/api.py
@@ -3,19 +3,17 @@ import json
 import random
 
 def get_data(location, term=None):
-    api_key = '6MqGGOGfbw44AovX18OKlkUjdb1UBAxui9s03uPsZ4HLaJ7Ubru5ov9IUR8xJBYCVohnaz\
-    SYRM_PUJtq-4a4hvSFbPMpXL9DQpuoBL4b4o9vedDZzJYMd4x4K0TIXXYx'
+    api_key = '6MqGGOGfbw44AovX18OKlkUjdb1UBAxui9s03uPsZ4HLaJ7Ubru5ov9IUR8xJBYCVohnazSYRM_PUJtq-4a4hvSFbPMpXL9DQpuoBL4b4o9vedDZzJYMd4x4K0TIXXYx'
     headers = {'Authorization': 'Bearer %s' % api_key}
 
     url = 'https://api.yelp.com/v3/businesses/search'
-    if term!=None:
+    if term!='':
         params = {'term': term, 'location': location}
     else:
         params = {'location': location}
 
     req = requests.get(url, params = params, headers = headers)
-    parsed = json.loads(req.txt)
-    businsses = parsed['businesses']
-
+    parsed = json.loads(req.text)
+    businesses = parsed['businesses']
     i = random.randint(0, len(businesses) - 1)
     return businesses[i]
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, request
 from app import app
 from app.forms import SearchForm
-import from .api import get_data
+from .api import get_data
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/search', methods = ['POST'])
@@ -10,12 +10,11 @@ def search():
     if form.validate_on_submit():
         location = request.form.get('location')
         term = request.form.get('search')
-        return redirect('/results/{}'.format(location))
+        return redirect('/results/{}/{}'.format(location, term))
     return render_template('search.html', form = form)
 
-@app.route('/results/<location>')
-def results(location):
-    print(location)
-    data = get_data(location)
-    print(data)
+@app.route('/results/<location>/<term>')
+def results(location, term):
+    business = get_data(location)
+    form = 
     return render_template('results.html')
